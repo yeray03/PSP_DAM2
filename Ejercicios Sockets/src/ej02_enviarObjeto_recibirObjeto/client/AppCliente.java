62f72689cfd1ee629e9ea46d09d27ee4596bc69f
Yeray <yeray.fermosomo@elorrieta-errekamari.com>
2025-12-02 21:19:22 +0100

    creado objeto persona e inicio de la app cliente

--- Ejercicios Sockets/src/ej02_enviarObjeto_recibirObjeto/client/AppCliente.java
+++ Ejercicios Sockets/src/ej02_enviarObjeto_recibirObjeto/client/AppCliente.java
@@ -1,5 +1,16 @@
 package ej02_enviarObjeto_recibirObjeto.client;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class AppCliente {
 
+    public static void main(String[] args) {
+        Socket socket = null;
+        ObjectInputStream ObjectInputStream = null;
+        ObjectOutputStream ObjectOutputStream = null;
+        String ipServer = "127.0.0.1";
+        int puertoServer = 49171;
+    }
 }
